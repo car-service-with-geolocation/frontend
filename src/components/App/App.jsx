0b893e66f75ae0cd9df0d28e2625ca28f24ccb8a
Яковлева Ксения <bezdenejnyhk@yandex.ru>
2023-09-24 21:00:05 +0300

    feat: add component Preloader

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,7 @@ import ServicePage from '../../Pages/ServicePage/ServicePages';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import style from './styles/App.module.css';
+import Preloader from '../Preloader/Preloader';
 
 function App() {
   return (
@@ -21,6 +22,7 @@ function App() {
         <Route path="/search" element={<MapPage />} />
       </Routes>
       <Footer />
+      <Preloader />
       {/* <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/login"
