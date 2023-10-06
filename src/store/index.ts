a53f410e79507a81730d0e6f6c67079c090225ea
Николай Николаенко <nikonikolaenko@yandex.ru>
2023-10-06 17:10:40 +0300

    refactor(components): refactor to TypeScript

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 
 import autoServiceByCoordSlice from './autoServiceByCoordSlice';
 import autoServiceIdSlice from './autoServiceIdSlice';
 import autoServiceSlice from './autoServicesSlice';
 import carsSlice from './carsSlice';
 
-// eslint-disable-next-line import/prefer-default-export
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+type DispatchFunc = () => AppDispatch;
+export const useAppDispatch: DispatchFunc = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 export const store = configureStore({
   reducer: {
     mainAutoServices: autoServiceSlice,
