e1f82dcc0b042fcb6ae19ec8c76021f0f7c9fc3c
Petr Kuzora <petr1725@yandex.ru>
2023-10-08 23:27:07 +0300

    feat(components): header, mainpage to mobile
    Старница MainPage и компонент Header приведены к мобильной версии

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 import carLogo from '../../images/car-Logo.svg';
@@ -5,36 +6,51 @@ import style from './styles/Header.module.css';
 
 function Header() {
   const location = useLocation();
+
+  const [isActive, setIsActive] = useState(false);
+
+  const handlerClick = () => {
+    setIsActive((current) => !current);
+  };
+
   return (
     <header className={style.header}>
       <div className={style.block}>
         <Link className={style.logoLink} to="/">
-          <img src={carLogo} alt="Логотип" />
+          <img className={style.logoImg} src={carLogo} alt="Логотип" />
           <p className={style.logoText}>Premium Car Services</p>
         </Link>
-        <div className={style.link}>
-          <ul className={style.menu}>
-            <li className={style.menu__item}>
-              <Link
-                className={`${style.linkText} ${
-                  location.pathname === '/search' ? style.active : ''
-                }`}
-                to="/search"
-              >
-                Поиск автосервисов
-              </Link>
-            </li>
-            <li className="menu__item">
-              <Link
-                className={`${style.linkText} ${
-                  location.pathname === '/forservices' ? style.active : ''
-                }`}
-                to="/forservices"
-              >
-                Для автосервисов
-              </Link>
-            </li>
-          </ul>
+        <div>
+          <button
+            onClick={handlerClick}
+            className={`${style.hum__icon} ${isActive ? style.menu_active : ''}`}
+          >
+            <span />
+          </button>
+          <div className={`${style.menu} ${isActive ? style.menu_active : ''}`}>
+            <ul className={style.menu__list}>
+              <li className={style.menu__item}>
+                <Link
+                  className={`${style.linkText} ${
+                    location.pathname === '/search' ? style.active : ''
+                  }`}
+                  to="/search"
+                >
+                  Поиск автосервисов
+                </Link>
+              </li>
+              <li className={style.menu__item}>
+                <Link
+                  className={`${style.linkText} ${
+                    location.pathname === '/forservices' ? style.active : ''
+                  }`}
+                  to="/forservices"
+                >
+                  Для автосервисов
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <p className={style.enterText}>Вход</p>
